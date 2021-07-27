1e53954f528e4eef8a59e59e982fb6724b9a8f3a
bitueboss <69850346+bitueboss@users.noreply.github.com>
2021-07-28 02:49:06 +0600

    first ever using tailwind css

---
+++ main.js
@@ -0,0 +1,10 @@
+// grad here
+
+const btn = document.getElementById("btn")
+const dis = document.getElementById("display")
+
+// action is here 
+
+btn.addEventListener("click" ,function(){
+    dis.classList.toggle("hidden")
+})
